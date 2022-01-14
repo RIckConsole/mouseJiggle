8aad8f9999b967801d10d3a3084af48a51d33e06
Rick Console <rickconsole@protonmail.com>
2022-01-13 22:07:25 -0500

    added dvd mode

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 var version = "1.0"
 var random = false
 var interval = 1
+var dvd = false
 
 func init() {
 	flaggy.SetName("Mouse Jiggle")
@@ -21,11 +22,15 @@ func init() {
 
 	flaggy.Bool(&random, "r", "random", "Enable random mouse movements (default is slight jitter)")
 	flaggy.Int(&interval, "i", "interval", "Set the time (in seconds) between mouse movements")
+	flaggy.Bool(&dvd, "d", "dvd", "Set the cursor to DVD mode")
 
 	flaggy.Parse()
 }
 
 func main() {
+	if dvd {
+		dvdmove()
+	}
 	fmt.Println("[*] Engaging Mouse Movements. Press CTRL+C to stop.")
 	if random {
 		i := 1
@@ -50,4 +55,25 @@ func main() {
 		time.Sleep(time.Duration(interval) * time.Second / 2)
 		robotgo.MoveSmoothRelative(-5, 0)
 	}
+}
+
+func dvdmove() {
+	sx, sy := robotgo.GetScreenSize()
+	i := true
+	mx := 2
+	my := 2
+	for i {
+		x, y := robotgo.GetMousePos()
+		if x >= sx-5 || x <= 5 {
+			mx = mx * -1
+		} else if y >= sy-5 || y <= 5 {
+			my = my * -1
+		} // else if (x <= -5 || x >= sx-5) && (y <= -5 || y >= sy-5) {
+		//fmt.Println("YEAHHHHHHH")
+		//}
+
+		robotgo.MoveSmoothRelative(mx, my, 0.0, 0.0)
+
+	}
+
 }
